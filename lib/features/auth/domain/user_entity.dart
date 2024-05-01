fefe1c18946533d430a292ea7b19f19336a32e80
Marc Peejay Viernes <mpvviernes@gmail.com>
2024-05-01 18:26:52 +0800

    dev: add core base classes

---
+++ lib/features/auth/domain/user_entity.dart
@@ -0,0 +1,9 @@
+import 'package:equatable/equatable.dart';
+
+class UserEntity extends Equatable {
+  const UserEntity();
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
