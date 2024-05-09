5caabb3b5f8cca1f85507ff04e3112f70720b362
Mark-Badua <markbadua92@gmail.com>
2024-05-09 17:30:32 +0800

    chore: comment out unused dart files

--- lib/features/create_schedule/data/repositories/maps_repository.dart
+++ lib/features/create_schedule/data/repositories/maps_repository.dart
@@ -1,121 +1,121 @@
-import 'package:revy/features/create_schedule/api_client/lib/api_client.dart';
-import '../../domain/entities.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:revy/features/create_schedule/api_client/lib/api_client.dart';
+// import '../../domain/entities.dart';
+// import 'package:geolocator/geolocator.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapsRepository {
-  MapsRepository({required this.apiClient});
+// class MapsRepository {
+//   MapsRepository({required this.apiClient});
 
-  final ApiClient apiClient;
+//   final ApiClient apiClient;
 
-  Future<Address?> fetchAddressFromUserCoordinates() async {
-    LocationPermission permission;
-    try {
-      permission = await Geolocator.checkPermission();
+//   Future<Address?> fetchAddressFromUserCoordinates() async {
+//     LocationPermission permission;
+//     try {
+//       permission = await Geolocator.checkPermission();
 
-      if (permission == LocationPermission.denied) {
-        permission = await Geolocator.requestPermission();
-      }
+//       if (permission == LocationPermission.denied) {
+//         permission = await Geolocator.requestPermission();
+//       }
 
-      if (permission == LocationPermission.deniedForever) {
-        return null;
-      }
+//       if (permission == LocationPermission.deniedForever) {
+//         return null;
+//       }
 
-      final position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high,
-      );
+//       final position = await Geolocator.getCurrentPosition(
+//         desiredAccuracy: LocationAccuracy.high,
+//       );
 
-      final addressData =
-          await apiClient.mapsResource.fetchAddressFromCoordinates(
-        latitude: position.latitude.toString(),
-        longitude: position.longitude.toString(),
-      );
+//       final addressData =
+//           await apiClient.mapsResource.fetchAddressFromCoordinates(
+//         latitude: position.latitude.toString(),
+//         longitude: position.longitude.toString(),
+//       );
 
-      return Address.fromJson(addressData['address']);
-    } catch (err) {
-      throw Exception('Failed to fetch user coordinates: $err');
-    }
-  }
+//       return Address.fromJson(addressData['address']);
+//     } catch (err) {
+//       throw Exception('Failed to fetch user coordinates: $err');
+//     }
+//   }
 
-  Future<Address> fetchAddressFromCoordinates({
-    required double latitude,
-    required double longitude,
-  }) async {
-    try {
-      final addressData =
-          await apiClient.mapsResource.fetchAddressFromCoordinates(
-        latitude: latitude.toString(),
-        longitude: longitude.toString(),
-      );
+//   Future<Address> fetchAddressFromCoordinates({
+//     required double latitude,
+//     required double longitude,
+//   }) async {
+//     try {
+//       final addressData =
+//           await apiClient.mapsResource.fetchAddressFromCoordinates(
+//         latitude: latitude.toString(),
+//         longitude: longitude.toString(),
+//       );
 
-      return Address.fromJson(addressData['address']);
-    } catch (err) {
-      throw Exception('Failed to fetch address from coordinates: $err');
-    }
-  }
+//       return Address.fromJson(addressData['address']);
+//     } catch (err) {
+//       throw Exception('Failed to fetch address from coordinates: $err');
+//     }
+//   }
 
-  Future<List<AddressSuggestion>> fetchAddressSuggestions({
-    required String query,
-    required double latitude,
-    required double longitude,
-  }) async {
-    try {
-      if (query.isEmpty) {
-        return [];
-      }
-      final payload = {
-        'query': query,
-        'latitude': latitude,
-        'longitude': longitude,
-      };
+//   Future<List<AddressSuggestion>> fetchAddressSuggestions({
+//     required String query,
+//     required double latitude,
+//     required double longitude,
+//   }) async {
+//     try {
+//       if (query.isEmpty) {
+//         return [];
+//       }
+//       final payload = {
+//         'query': query,
+//         'latitude': latitude,
+//         'longitude': longitude,
+//       };
 
-      final suggestionsData =
-          await apiClient.mapsResource.postAutocompleteSuggestionsRequest(
-        data: payload,
-      );
+//       final suggestionsData =
+//           await apiClient.mapsResource.postAutocompleteSuggestionsRequest(
+//         data: payload,
+//       );
 
-      return (suggestionsData['suggestions'] as List)
-          .map((suggestion) => AddressSuggestion.fromJson(suggestion))
-          .toList();
-    } catch (err) {
-      throw Exception('Failed to fetch : $err');
-    }
-  }
+//       return (suggestionsData['suggestions'] as List)
+//           .map((suggestion) => AddressSuggestion.fromJson(suggestion))
+//           .toList();
+//     } catch (err) {
+//       throw Exception('Failed to fetch : $err');
+//     }
+//   }
 
-  Future<Address?> fetchAddressFromPlaceId({required String placeId}) async {
-    try {
-      final placeData = await apiClient.mapsResource.fetchPlaceDetails(
-        placeId: placeId,
-      );
+//   Future<Address?> fetchAddressFromPlaceId({required String placeId}) async {
+//     try {
+//       final placeData = await apiClient.mapsResource.fetchPlaceDetails(
+//         placeId: placeId,
+//       );
 
-      return Address.fromJson(placeData['place']);
-    } catch (err) {
-      throw Exception('Failed to fetch address from place ID: $err');
-    }
-  }
+//       return Address.fromJson(placeData['place']);
+//     } catch (err) {
+//       throw Exception('Failed to fetch address from place ID: $err');
+//     }
+//   }
 
-  Future<List<LatLng>> fetchDirections({
-    required String originId, //PlaceIds
-    required String destinationId,
-  }) async {
-    try {
-      final directionsData = await apiClient.mapsResource.postDirectionsRequest(
-        originId: originId,
-        destinationId: destinationId,
-      );
-      return directionsData['directions']['route']
-          .map(
-            (direction) {
-              return LatLng(
-                direction['latitude'],
-                direction['longitude'],
-              );
-            },
-          )
-          .toList()
-          .cast<LatLng>();
-    } catch (err) {
-      throw Exception('Failed to fetch address from place ID: $err');
-    }
-  }
-}
+//   Future<List<LatLng>> fetchDirections({
+//     required String originId, //PlaceIds
+//     required String destinationId,
+//   }) async {
+//     try {
+//       final directionsData = await apiClient.mapsResource.postDirectionsRequest(
+//         originId: originId,
+//         destinationId: destinationId,
+//       );
+//       return directionsData['directions']['route']
+//           .map(
+//             (direction) {
+//               return LatLng(
+//                 direction['latitude'],
+//                 direction['longitude'],
+//               );
+//             },
+//           )
+//           .toList()
+//           .cast<LatLng>();
+//     } catch (err) {
+//       throw Exception('Failed to fetch address from place ID: $err');
+//     }
+//   }
+// }
