842eff79f9c56c0a2bd8ab8cecebc7d78eaaac92
Mark-Badua <markbadua92@gmail.com>
2024-05-15 14:54:55 +0800

    feat: add search bar screen

---
+++ lib/features/create_schedule/application/presentation/screens/search_bar_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'select_starting_location.dart';
+import '../widgets/search_bar.dart';
+
+class SearchBarScreen extends StatelessWidget {
+  const SearchBarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        title: const Text("Create Schedule"),
+      ),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 60.0), // Add padding to avoid overlap
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  // Text widget
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      "Where will you be coming from? Search your address",
+                      style: TextStyle(fontSize: 22),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  // Search Bar widget with Places API
+                  Container(
+                    height: 500,
+                    child: const PlacesSearchBar(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // Button for next stage of create schedule
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0), // Padding for aesthetic spacing
+              child: Row(
+                children: [
+                  Expanded(
+                    child: FloatingActionButton.extended(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const StartLocScreen()),
+                        );
+                      },
+                      label: const Text('Select Starting Location'),
+                      backgroundColor: Colors.green,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
